a96d5eece427a45323ab0ead088dcab7941e404b
Peter Borcsok <peter.borcsok@szeged.cas.de>
2023-10-26 11:49:58 +0200

    Prepare for TDD

--- tests/decode.test.ts
+++
@@ -1,9 +0,0 @@
-import { decode } from "../src/decode";
-
-describe('Testing decode', () => {
-
-    test('it works', () => {
-        expect(decode([1], 0)).toEqual('a');
-    });
-
-})
--- tests/find-key.test.ts
+++
@@ -1,9 +0,0 @@
-import { findTheKey } from '../src/find-key';
-
-describe('Testing find the key', () => {
-
-    test('it works', () => {
-        expect(findTheKey('a', [])).toBe(0);
-    })
-
-})
